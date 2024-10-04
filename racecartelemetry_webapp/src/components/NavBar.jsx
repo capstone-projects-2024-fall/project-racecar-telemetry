ecd5d4a8166d0478ba1f2868b2c4a0a909312e98
Arjun Patel <tuo45748@temple.edu>
2024-10-04 16:07:28 -0400

    forgot the changes i made since last commit

--- racecartelemetry_webapp/src/components/NavBar.jsx
+++ racecartelemetry_webapp/src/components/NavBar.jsx
@@ -4,6 +4,7 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import Link from "next/link";
+import { red } from "@mui/material/colors";
 
 const Navbar = () => {
   const handleLogout = () => {
@@ -12,27 +13,23 @@ const Navbar = () => {
   };
 
   return (
-    <AppBar
-      position="static"
-      sx={{ backgroundColor: "#2c3e50", boxShadow: 99 }}
-    >
-      <Toolbar>
+    <AppBar position="static" >
+      <Toolbar
+        sx={{ background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)", marginBottom:2 }}
+      >
         <Box sx={{ flexGrow: 1 }}>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            navbar
+            Temple Formula Racing
           </Typography>
         </Box>
 
         <Link href="/" passHref>
-          <Button color="inherit">Dashboard</Button>
+          <Button color="white">Dashboard</Button>
         </Link>
         <Link href="/profiles" passHref>
-          <Button color="inherit">Profiles</Button>
+          <Button color="white">Profiles</Button>
         </Link>
-        <Button
-          sx={{ color: "white", backgroundColor: "#e74c3c" }}
-          onClick={handleLogout}
-        >
+        <Button sx={{ color: "white", border: 2 }} onClick={handleLogout}>
           Logout
         </Button>
       </Toolbar>
