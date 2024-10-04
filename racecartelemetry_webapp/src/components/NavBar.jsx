42153b00fc9ace4fa6e754a5c077a44376d80eef
Arjun Patel <tuo45748@temple.edu>
2024-10-04 15:14:31 -0400

    color

--- racecartelemetry_webapp/src/components/NavBar.jsx
+++ racecartelemetry_webapp/src/components/NavBar.jsx
@@ -1,19 +1,21 @@
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import Link from 'next/link';
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Link from "next/link";
 
 const Navbar = () => {
-
   const handleLogout = () => {
-    alert('You have been logged out!');
+    alert("You have been logged out!");
     // replacee
   };
 
   return (
-    <AppBar position="static">
+    <AppBar
+      position="static"
+      sx={{ backgroundColor: "#2c3e50", boxShadow: 99 }}
+    >
       <Toolbar>
         <Box sx={{ flexGrow: 1 }}>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
