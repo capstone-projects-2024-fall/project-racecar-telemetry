385148cbcc3b180d54d9ae86efff27f5058bc23c
Arjun Patel <tuo45748@temple.edu>
2024-10-15 10:58:49 -0400

    check window before initializing firebase

--- racecartelemetry_webapp/src/firebase/firebaseConfig.js
+++ racecartelemetry_webapp/src/firebase/firebaseConfig.js
@@ -2,21 +2,29 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBxEzUYz67esB6pM9DdDlFG_Z-aoqMXBT4",
-    authDomain: "race-car-telemetry.firebaseapp.com",
-    projectId: "race-car-telemetry",
-    storageBucket: "race-car-telemetry.appspot.com",
-    messagingSenderId: "819026721519",
-    appId: "1:819026721519:web:3383f6afbdd27e633cb91a",
-    measurementId: "G-9VZ794ZRFC"
-  };
+let app;
+let database;
+let analytics;
 
-const app = initializeApp(firebaseConfig);
+if (typeof window !== "undefined") {
+    const firebaseConfig = {
+        apiKey: "AIzaSyBxEzUYz67esB6pM9DdDlFG_Z-aoqMXBT4",
+        authDomain: "race-car-telemetry.firebaseapp.com",
+        projectId: "race-car-telemetry",
+        storageBucket: "race-car-telemetry.appspot.com",
+        messagingSenderId: "819026721519",
+        appId: "1:819026721519:web:3383f6afbdd27e633cb91a",
+        measurementId: "G-9VZ794ZRFC"
+    };
+    
 
-const database = getDatabase(app);
+  app = initializeApp(firebaseConfig);
+  database = getDatabase(app);
 
-const analytics = getAnalytics(app);
+  if (window.location.hostname !== "localhost") {
+    analytics = getAnalytics(app);
+  }
+}
 
 export { app, database, analytics };
 
