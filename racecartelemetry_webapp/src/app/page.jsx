b129282c84b097e12a44be9f453cd3eaa66f6811
NasierF <119966231+NasierF@users.noreply.github.com>
2024-10-18 16:00:22 -0400

    added listener for real time database - LIVE DATA READING ID FUNCTIONAL

--- racecartelemetry_webapp/src/app/page.jsx
+++ racecartelemetry_webapp/src/app/page.jsx
@@ -6,6 +6,7 @@ import Button from "@mui/material/Button";
 import NavBar from "@components/Navbar";
 import DataDisplay from "@components/DataDisplay";
 import TestData from "@components/testData"
+import CANDataLiveReading from "@components/CANDataLiveReading"
 
 export default function Home() {
   const telemetryData = [
@@ -20,6 +21,7 @@ export default function Home() {
       <DataDisplay data={telemetryData} />
       homepage add stuff
       <TestData/>
+      <CANDataLiveReading canID={'001'}/>
     </div>
   );
 }
