d00610284fc341fff7b66304b923305fb4abd7c3
Arjun Patel <tuo45748@temple.edu>
2024-10-24 00:10:35 -0400

    test

--- racecartelemetry_webapp/src/components/DataDisplay.jsx
+++ racecartelemetry_webapp/src/components/DataDisplay.jsx
@@ -8,7 +8,7 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 
-const DataDisplay = ({ data }) => {
+const DataDisplay = ({ datas }) => {
   return (
     <TableContainer
       component={Paper}
