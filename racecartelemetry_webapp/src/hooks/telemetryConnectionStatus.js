ee06f787593a45fb5de9f7c3f83420a7907d105f
Jacky <59948446+WJ2K@users.noreply.github.com>
2024-10-31 11:35:41 -0400

    update connection status

--- racecartelemetry_webapp/src/hooks/telemetryConnectionStatus.js
+++ racecartelemetry_webapp/src/hooks/telemetryConnectionStatus.js
@@ -14,39 +14,28 @@ const telemetryConnectionStatus = () => {
 
     const unsubscribe = onValue(connectedRef, (snapshot) => {
       const currentData = snapshot.val()
-      console.log(currentData)
+      const currentDataString = JSON.stringify(currentData)
 
-      if (currentData) {
-        const currentDataString = JSON.stringify(currentData)
+      if (previousDataRef.current === null) {
+        previousDataRef.current = currentDataString
+      }
 
-        if (previousDataRef.current === null) {
-          previousDataRef.current = currentDataString
+      if (currentData && previousDataRef.current !== currentDataString) {
+        setIsConnected(true)
+        previousDataRef.current = currentDataString
+
+        if (timeoutRef.current) {
+          clearTimeout(timeoutRef.current)
         }
 
-        if (previousDataRef.current !== currentDataString) {
-          // New data detected, set to connected
-          setIsConnected(true)
-
-          // Update the previous data with the new data
-          previousDataRef.current = currentDataString
-
-          // Reset the disconnection timeout
-          if (timeoutRef.current) {
-            clearTimeout(timeoutRef.current)
-          }
-
-          // Set a timeout to detect disconnection if no new data arrives
-          timeoutRef.current = setTimeout(() => {
-            setIsConnected(false) // Timeout expired, set to disconnected
-          }, 5000) // Adjust the timeout duration as needed (5000 ms here)
-        }
+        timeoutRef.current = setTimeout(() => {
+          setIsConnected(false)
+        }, 5000)
       } else {
-        // No data present, set as disconnected
         setIsConnected(false)
       }
     })
 
-    // Cleanup function
     return () => {
       unsubscribe()
       if (timeoutRef.current) {
