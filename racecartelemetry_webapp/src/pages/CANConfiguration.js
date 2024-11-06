fbac304089aaba9ae65f9361c454fd9bae21abea
NasierF <119966231+NasierF@users.noreply.github.com>
2024-11-06 15:45:03 -0500

    stream

--- racecartelemetry_webapp/src/pages/CANConfiguration.js
+++ racecartelemetry_webapp/src/pages/CANConfiguration.js
@@ -3,12 +3,15 @@
 import React, { useState } from 'react'
 import CANDataAssignment from '@components/CANDataAssignment'
 import ConfigManager from '@components/ConfigManager'
+import Box from '@mui/material/Box';
+
 
 export default function CANConfigurationPage() {
   const [selectedConfig, setSelectedConfig] = useState("")
 
   return (
     <>
+
       <ConfigManager onConfigSelect = {setSelectedConfig} />
       {selectedConfig && <CANDataAssignment selectedConfig = {selectedConfig}/>}
     </>
